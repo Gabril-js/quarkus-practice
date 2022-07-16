e4febca88f3af70f0a5021325d6d0e376e3dbe5a
Gabril-js <gabriel.gl2302@gmail.com>
2022-07-16 01:39:59 -0300

    feat: model

---
+++ src/main/java/org/acme/Model/Person.java
@@ -0,0 +1,10 @@
+package org.acme.Model;
+
+import javax.persistence.Entity;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+
+@Entity
+public class Person extends PanacheEntityBase{
+    
+}
