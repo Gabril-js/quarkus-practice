252bfab0548b6ca38dfeaf653a5b4ea6fa0ba38d
Gabril-js <gabriel.gl2302@gmail.com>
2022-07-17 16:12:10 -0300

    fix: get and set methods replaced by "@data" lombok

--- src/main/java/org/acme/DTO/PersonDTO.java
+++ src/main/java/org/acme/DTO/PersonDTO.java
@@ -3,6 +3,9 @@ package org.acme.dto;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 
+import lombok.Data;
+
+@Data
 public class PersonDto {
 
     @NotNull
@@ -15,39 +18,4 @@ public class PersonDto {
     @Email
     private String email;
 
-    public String getName() {
-        return name;
-
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public Long getCpf() {
-        return cpf;
-
-    }
-
-    public void setCpf(Long cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getEmail() {
-        return email;
-
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
