05263ed6c21368714b59e916d49efa3f7cfcddd3
Gabril-js <gabriel.gl2302@gmail.com>
2022-07-17 00:17:25 -0300

    feat: DTO

--- src/main/java/org/acme/DTO/PersonDTO.java
+++ src/main/java/org/acme/DTO/PersonDTO.java
@@ -1,5 +1,53 @@
-package org.acme.DTO;
+package org.acme.dto;
 
-public class PersonDTO {
-    
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+
+public class PersonDto {
+
+    @NotNull
+    private String name;
+
+    private Integer age;
+
+    private Long cpf;
+
+    @Email
+    private String email;
+
+    public String getName() {
+        return name;
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Long getCpf() {
+        return cpf;
+
+    }
+
+    public void setCpf(Long cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
