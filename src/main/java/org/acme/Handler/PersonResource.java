282b0ab0593f87d4549d94052ea9249fa0ca633c
Gabril-js <gabriel.gl2302@gmail.com>
2022-07-17 00:15:58 -0300

    fix: Resource

--- src/main/java/org/acme/Handler/PersonResource.java
+++
@@ -1,9 +0,0 @@
-package org.acme.Handler;
-
-import org.acme.Model.Person;
-
-import io.quarkus.hibernate.orm.rest.data.panache.PanacheEntityResource;
-
-public interface PersonResource extends PanacheEntityResource<Person, Long>{
-    
-}
