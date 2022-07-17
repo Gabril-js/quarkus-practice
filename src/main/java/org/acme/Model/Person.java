a0d22cc25879916a11ee328686a6811dd536df6a
Gabril-js <gabriel.gl2302@gmail.com>
2022-07-17 00:17:50 -0300

    feat: Model

--- src/main/java/org/acme/Model/Person.java
+++ src/main/java/org/acme/Model/Person.java
@@ -1,10 +1,28 @@
 package org.acme.Model;
 
 import javax.persistence.Entity;
+import javax.validation.constraints.Email;
 
-import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-public class Person extends PanacheEntityBase{
-    
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person extends PanacheEntity {
+
+    private String name;
+
+    private Integer age;
+
+    private Long cpf;
+
+    @Email
+    private String email;
+
 }
