ae58a775ddff0e9001b8f8e1a247e199b0bb1276
Gabril-js <gabriel.gl2302@gmail.com>
2022-07-17 00:18:39 -0300

    fix: Repository

--- src/main/java/org/acme/Repository/PersonRepository.java
+++ src/main/java/org/acme/Repository/PersonRepository.java
@@ -1,12 +1,9 @@
 package org.acme.Repository;
 
-import javax.enterprise.context.ApplicationScoped;
-
 import org.acme.Model.Person;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
-@ApplicationScoped
-public class PersonRepository implements PanacheRepository<Person>{
-    
+public class PersonRepository implements PanacheRepository<Person> {
+
 }
